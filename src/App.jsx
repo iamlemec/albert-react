dbd5b4b79c537de2cefda7eb93bb6a63c2d33dcb
Douglas Hanley <thesecretaryofwar@gmail.com>
2023-12-12 13:26:26 -0500

    add in muted input

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@ import { useState, useEffect, useRef } from 'react'
 import QueryBubble from './components/QueryBubble'
 import './App.css'
 
+const msg0 = ['system', 'Hello, I am Ziggy!', '']
+
 export default function App() {
-    const [messages, setMessages] = useState([])
+    const [streaming, setStreaming] = useState(false)
+    const [messages, setMessages] = useState([msg0])
     const [query, setQuery] = useState('')
     const messagesEndRef = useRef(null)
+    const inputRef = useRef(null)
 
     function appendReply(msgs, value) {
         const [type, query, reply] = msgs[msgs.length-1];
@@ -14,6 +18,9 @@ export default function App() {
     }
     
     async function sendMessage() {
+        // set streaming bit
+        setStreaming(true)
+
         // send request to server
         let response = await fetch('http://localhost:8000/query', {
             method: 'POST',
@@ -31,6 +38,10 @@ export default function App() {
         for await (const value of reader) {
             setMessages(msgs => appendReply(msgs, value));
         }
+
+        // clear query
+        setStreaming(false)
+        setQuery('')
     }
 
     function onKeyDown(e) {
@@ -47,8 +58,14 @@ export default function App() {
         scrollToBottom()
     }, [messages]);
 
+    useEffect(() => {
+        if (!streaming) {
+            inputRef.current?.focus()
+        }
+    }, [streaming]);
+
     return (
-        <div className="flex flex-col items-center w-full h-full">
+        <div className="flex flex-col justify-end items-center w-full h-full">
             <div id="chat-box" className="text-lg pt-[150px] pb-[250px] flex flex-col items-center w-[1200px] overflow-scroll no-scrollbar">
                 <div id="message-list" className="flex flex-col gap-8 w-full">
                     {messages.map(([type, query, reply], index) => (
@@ -60,8 +77,11 @@ export default function App() {
             <div id="input-area" className="absolute bottom-32 m-auto w-[800px]">
                 <input
                     type="text"
-                    placeholder="Type your message here"
-                    className="text-lg p-3.5 rounded border border-[#666] shadow-md focus:outline-none w-full"
+                    autoFocus
+                    ref={inputRef}
+                    placeholder="Type your query here..."
+                    className="text-md font-mono p-3 rounded border border-[#666] shadow-md focus:outline-none w-full"
+                    disabled={streaming}
                     value={query}
                     onChange={(e) => setQuery(e.target.value)}
                     onKeyDown={(e) => onKeyDown(e)}
--- src/components/QueryBubble.jsx
+++ src/components/QueryBubble.jsx
@@ -1,11 +1,13 @@
 export default function QueryBubble({ type, query, reply }) {
     return (
         <div className="flex flex-col border border-[#666] w-full rounded-[4px]">
-            <div className="flex flex-row bg-[#eee] border-b border-[#666] rounded-t-[4px]">
+            <div className="flex flex-row bg-[#eee] rounded-t-[4px] last:rounded-b-[4px]">
                 <div className="w-24 text-center smallcaps font-bold border-r border-[#666] p-3">{type}</div>
                 <div className="p-3">{query}</div>
             </div>
-            <div className="p-3">{reply}</div>
+            {reply.length > 0 &&
+                <div className="p-3 border-t border-[#666]">{reply}</div>
+            }
         </div>
     )
 }
